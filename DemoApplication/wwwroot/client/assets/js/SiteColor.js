5ff9ec752b2cc6fbff12e38cb53d53d01a3d8e69
KamilMammadov <kamiltm@code.edu.az>
2022-12-12 03:20:12 +0400

    SiteColor

---
+++ DemoApplication/wwwroot/client/assets/js/SiteColor.js
@@ -0,0 +1,32 @@
+﻿
+
+
+
+console.log("salam")
+
+const cookieObj = new URLSearchParams(document.cookie.replaceAll("&", "%26").replaceAll("; ", "&"))
+var color = cookieObj.get("SiteColors")
+console.log(color)
+
+let obj = JSON.parse(color);
+
+document.body.style.backgroundColor = obj.Name
+
+//const uri = '%7B%22Id%22%3A3%2C%22Name%22%3A%22green%22%7D';
+//const encoded = encodeURI(uri);
+//console.log(encoded);
+//// expected output: "https://mozilla.org/?x=%D1%88%D0%B5%D0%BB%D0%BB%D1%8B"
+
+//try {
+//    console.log(decodeURI(encoded));
+//    // expected output: "https://mozilla.org/?x=шеллы"
+//} catch (e) { // catches a malformed URI
+//    console.error(e);
+//}
+
+
+//Id"%3A1%2C"Name"%3A"black
+
+
+
+
